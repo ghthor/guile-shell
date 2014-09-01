36c5aeb812ca6f2dffe6ff4c7f37042095ade62e
Willa Walthall <ghthor@gmail.com>
2014-09-01 05:06:27 -0400

    A []string -> c **char array conversion utility | src: https://groups.google.com/forum/#!topic/golang-nuts/pQueMFdY0mk

---
+++ charArray.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+
+char** MakeCharArray(int size) {
+    return calloc(sizeof(char*), size);
+}
+
+void SetArrayString(char **a, char *s, int n) {
+        a[n] = s;
+}
+
+void FreeCharArray(char **a, int size) {
+    int i;
+    for (i = 0; i < size; i++) {
+        free(a[i]);
+    }
+    free(a);
+}
---
+++ charArray.h
@@ -0,0 +1,3 @@
+extern char** MakeCharArray(int size);
+extern void SetArrayString(char **a, char *s, int n);
+extern void FreeCharArray(char **a, int size);
